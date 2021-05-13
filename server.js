281c12e90d75c12f67248d77c88e7da7df87e3ea
Sandip136697 <sandipburnwal800@gmail.com>
2021-05-13 13:16:09 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ connectDB();
 app.use(express.json({ extended: false}));
 
 
-//app.get('/', (req,res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users',users);
+app.use('/api/auth',auth);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
+//Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
     app.use(express.static('client/build'));
@@ -19,10 +26,5 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
     });
 }
-app.use('/api/users',users);
-app.use('/api/auth',auth);
-app.use('/api/profile',profile);
-app.use('/api/posts',posts);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
